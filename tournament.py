e9885fd12a5fe633904e6729d15b255c5a3d3d21
Nick Martinez <23nicholasmartinez@gmail.com>
2017-03-28 16:12:37 -0700

    Add methods to register and count players

--- tournament.py
+++ tournament.py
@@ -28,3 +28,34 @@ def deletePlayers():
     db_cursor.execute(query)
     conn.commit()
     conn.close()
+
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    conn = connect()
+    db_cursor = conn.cursor()
+    query = "SELECT COUNT(id) FROM players;"
+    db_cursor.execute(query)
+    results = db_cursor.fetchone()
+    conn.close()
+    if results:
+        return results[0]
+    else:
+        return '0'
+
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    conn = connect()
+    db_cursor = conn.cursor()
+    query = "INSERT INTO players (name) VALUES (%s)"
+    db_cursor.execute(query, (name,))
+    conn.commit()
+    conn.close()
